35ffe7a0b85dd6e756fc6e4a4e80556776c6cab7
Joe Fusco <joe.fusco@wpengine.com>
2024-01-22 14:45:12 -0500

    Resolving linting issues
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,27 +1,26 @@
+/* global WPGRAPHQL_IDE_DATA */
 import { GraphiQL } from 'graphiql';
 import 'graphiql/graphiql.min.css';
 
-const fetcher = async (graphQLParams) => {
-    const { graphqlEndpoint } = WPGRAPHQL_IDE_DATA;
+const fetcher = async ( graphQLParams ) => {
+	const { graphqlEndpoint } = WPGRAPHQL_IDE_DATA;
 
-    const response = await fetch(graphqlEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(graphQLParams),
-        credentials: 'same-origin' // or 'include' if your endpoint is on a different domain
-    });
+	const response = await fetch( graphqlEndpoint, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify( graphQLParams ),
+		credentials: 'same-origin', // or 'include' if your endpoint is on a different domain
+	} );
 
-    return response.json();
+	return response.json();
 };
 
 export function Editor() {
-    return (
-        <>
-            <GraphiQL
-                fetcher={fetcher}
-            />
-        </>
-    )
+	return (
+		<>
+			<GraphiQL fetcher={ fetcher } />
+		</>
+	);
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
+/* global WPGRAPHQL_IDE_DATA */
 import { createRoot } from '@wordpress/element';
 import { App } from './App';
 
 /**
  * Get the ID of the HTML element where the React app will be placed.
  *
- * @const {string} rootElementId - The ID of the HTML element.
+ * @constant {string} rootElementId - The ID of the HTML element.
  *
  * Localized in wpgraphql-ide.php
  */
 const { rootElementId } = WPGRAPHQL_IDE_DATA;
 
-const rootElement = document.getElementById(rootElementId);
+const rootElement = document.getElementById( rootElementId );
 
-if (rootElement) {
-    const root = createRoot(rootElement);
-    root.render(<App />);
+if ( rootElement ) {
+	const root = createRoot( rootElement );
+	root.render( <App /> );
 }
 
 // Expose app as a global variable to utilize in gutenberg.
