37048ccd22b95defe751a39df39975175cc78ddf
Joe Fusco <joe.fusco@wpengine.com>
2024-02-15 12:49:17 -0500

    Refactor E2E
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- playwright.config.js
+++ playwright.config.js
@@ -1,28 +1,12 @@
-const { defineConfig, devices } = require('@playwright/test');
-
-module.exports = defineConfig({
-  testDir: './tests/e2e',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+const config = {
   use: {
-    trace: 'on-first-retry',
-    baseURL: 'http://localhost:8888',
+    baseURL: 'http://localhost:8888/',
+    browserName: 'chromium',
+    headless: true,
+    viewport: { width: 1280, height: 720 },
   },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
-});
+  testDir: 'tests/e2e',
+  timeout: 30000,
+};
+
+module.exports = config;
--- tests/e2e/drawer.spec.js
+++ tests/e2e/drawer.spec.js
@@ -1,159 +1,74 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
-// Set up environment variables or a config file for sensitive info
-const wpAdminUrl = 'http://localhost:8888/wp-admin';
-const wpHomeUrl = 'http://localhost:8888';
-const adminUsername = 'admin';
-const adminPassword = 'password';
+const selectors = {
+  loginUsername: '#user_login',
+  loginPassword: '#user_pass',
+  submitButton: '#wp-submit',
+  graphiqlContainer: '.graphiql-container',
+  graphiqlResponse: '.graphiql-response',
+  graphiqlDocExplorer: '.graphiql-doc-explorer',
+  editorDrawerButton: '.EditorDrawerButton',
+  editorDrawerCloseButton: '.EditorDrawerCloseButton',
+  executeQueryButton: '.graphiql-execute-button',
+  queryInput: '.graphiql-editor',
+};
 
 test.beforeEach(async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await page.fill('input[name="log"]', adminUsername);
-  await page.fill('input[name="pwd"]', adminPassword);
-  await page.click('input[type="submit"]');
-
-  await visitPluginsPage(page);
-  const activateWPGraphQL = await page.$(`id=activate-wp-graphql`);
-  if (activateWPGraphQL) {
-    await activateWPGraphQL.click();
-  }
-
-  const activateWPGraphQLIDE = await page.$(`id=activate-wpgraphql-ide`);
-  if (activateWPGraphQLIDE) {
-    await activateWPGraphQLIDE.click();
-  }
+  await page.goto('http://localhost:8888/wp-admin');
+  await page.fill(selectors.loginUsername, 'admin');
+  await page.fill(selectors.loginPassword, 'password');
+  await page.click(selectors.submitButton);
 });
 
-test('drawer opens on an admin page', async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await openDrawer(page);
-  await expect(page.locator('.graphiql-container')).toBeVisible();
+test('should open and close the WPGraphQL IDE drawer', async ({ page }) => {
+  await page.click(selectors.editorDrawerButton);
+  await expect(page.locator(selectors.graphiqlContainer)).toBeVisible();
+  await page.click(selectors.editorDrawerCloseButton);
+  await expect(page.locator(selectors.graphiqlContainer)).not.toBeVisible();
 });
 
-test('drawer opens on a public page', async ({ page }) => {
-  await visitPublicFacingPageAsLoggedInUser(page);
-  await openDrawer(page);
-  await expect(page.locator('.graphiql-container')).toBeVisible();
+test('should execute a GraphQL query successfully', async ({ page }) => {
+  await page.click(selectors.editorDrawerButton);
+  await expect(page.locator(selectors.graphiqlContainer)).toBeVisible();
+  await typeQuery(page, '{posts{nodes{databaseId}}}');
+  await clickExecuteQueryButton(page);
+  const expectedResponseText = `
+    "data": {
+      "posts": {
+        "nodes": [
+          {
+            "databaseId": 1
+          }
+        ]
+      }
+    }`;
+  await expect(page.locator(selectors.graphiqlResponse)).toContainText(expectedResponseText);
 });
 
-test('drawer closes when close button is clicked', async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await openDrawer(page);
-  await expect(page.locator('.graphiql-container')).toBeVisible();
-  await clickDrawerCloseButton(page);
-  await expect(page.locator('.graphiql-container')).toBeHidden();
-});
-
-test('loads with the documentation explorer closed', async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await openDrawer(page);
-  await expect(page.locator('.graphiql-container')).toBeVisible();
-  await expect(page.locator('.graphiql-doc-explorer')).toBeHidden();
-});
-
-test('documentation explorer can be toggled open and closed', async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await openDrawer(page);
-  await expect(page.locator('.graphiql-container')).toBeVisible();
-  await page.click('[aria-label="Show Documentation Explorer"]');
-  await expect(page.locator('.graphiql-doc-explorer')).toBeVisible();
-  await page.click('[aria-label="Hide Documentation Explorer"]');
-  await expect(page.locator('.graphiql-doc-explorer')).toBeHidden();
-});
-
-test('executes query', async ({ page }) => {
-  await visitAdminFacingPage(page);
-  await openDrawer(page);
-  await typeQuery(page, `{posts{nodes{id}}}`);
-  await typeVariables(page, { first: 10 });
-  await executeQuery(page);
-  await page.waitForTimeout(1000);
-  const responseContent = await page.textContent('.graphiql-response'); // Adjust the selector as needed
-  expect(responseContent).toContain('posts');
-  expect(responseContent).toContain('nodes');
-});
-
-// Helper methods
-
-export async function clickDrawerButton(page) {
-  await page.click('.EditorDrawerButton');
-}
-
-export async function clickDrawerCloseButton(page) {
-  await page.click('.EditorDrawerCloseButton');
-}
-
-export async function visitAdminFacingPage(page) {
-  await page.goto(wpAdminUrl);
-}
-
-export async function visitPublicFacingPageAsLoggedInUser(page) {
-  await page.goto(wpHomeUrl);
-}
-
-export async function visitPluginsPage(page) {
-  await page.goto(`${wpAdminUrl}/plugins.php`);
-}
-
-async function setQuery(page, query) {
-  await page.evaluate((query) => {
-      localStorage.setItem('graphiql:query', query);
-  }, query);
-}
-
-async function setVariables(page, variables) {
-  const variablesString = JSON.stringify(variables);
-  await page.evaluate((variablesString) => {
-      localStorage.setItem('graphiql:variables', variablesString);
-  }, variablesString);
-}
-
-export async function executeQuery(page) {
+export async function clickExecuteQueryButton(page) {
   await page.click('.graphiql-execute-button');
+  // Wait for the spinner to disappear, indicating the query has finished executing
+  await page.waitForSelector('.graphiql-spinner', { state: 'hidden' });
 }
 
-async function typeQuery(page, query) {
-  const querySelector = '[aria-label="Query Editor"] .graphiql-editor .cm-s-graphiql';
-  await selectAndClearTextUsingKeyboard(page, querySelector);
-  await page.type(querySelector, query);
+export async function typeQuery(page, query) {
+  const querySelector = '[aria-label="Query Editor"] .CodeMirror';
+  await page.click(querySelector);
+  await clearCodeMirror(page, querySelector);
+  await page.keyboard.type(query);
 }
 
-async function typeVariables(page, variables) {
+export async function typeVariables(page, variables) {
+  const variablesString = JSON.stringify(variables, null, 2);
+  const variablesSelector = '[aria-label="Variables"] .CodeMirror';
   await page.click('[data-name="variables"]');
-  const variablesString = JSON.stringify(variables);
-  const variablesSelector = '[aria-label="Variables"] .graphiql-editor .cm-s-graphiql';
-  await selectAndClearTextUsingKeyboard(page, variablesSelector);
-  await page.type(variablesSelector, variablesString);
+  await clearCodeMirror(page, variablesSelector);
+  await page.keyboard.type(variablesString);
 }
 
-async function selectAndClearTextUsingKeyboard(page, selector) {
-  await page.click(selector); // Focus the element
-
-  // Determine the operating system to use the correct "Select All" shortcut
+async function clearCodeMirror(page, selector) {
+  await page.click(selector);
   const selectAllCommand = process.platform === 'darwin' ? 'Meta+A' : 'Control+A';
-  await page.keyboard.press(selectAllCommand); // Select all text using OS-specific shortcut
-  await page.keyboard.press('Backspace'); // Clear selected text
-}
-
-export async function openDrawer(page) {
-  const isDrawerVisible = await page.locator('.graphiql-container').isVisible();
-  if (!isDrawerVisible) {
-    await clickDrawerButton(page);
-    await page.waitForSelector('.graphiql-container', { state: 'visible' });
-  }
-}
-
-export async function closeDrawer(page) {
-  const isDrawerVisible = await page.locator('.graphiql-container').isVisible();
-
-  if (isDrawerVisible) {
-    const overlay = await page.locator('[vaul-overlay]');
-    if (overlay) {
-      await overlay.click();
-    }
-    await expect(page.locator('.graphiql-container')).toBeHidden();
-    await clickDrawerCloseButton(page);
-    await page.waitForSelector('.graphiql-container', { state: 'hidden' });
-  }
+  await page.keyboard.press(selectAllCommand);
+  await page.keyboard.press('Backspace');
 }
