0d6f46f8bdb5d75185636542246446d9230ce180
Jason Bahl <jasonbahl@mac.com>
2024-02-28 10:00:43 -0700

    - scaffold tests for GraphiQL Toolbar

---
+++ tests/unit/specs/GraphiQLToolbar.test.js
@@ -0,0 +1,20 @@
+/**
+ * @todo should this test live near the component it is testing?
+ *
+ * related: https://github.com/wp-graphql/wpgraphql-ide/issues/31
+ */
+describe('GraphiQL Toolbar', () => {
+
+	test('it should render', () => {})
+	test('it should render with prettify button', () => {})
+
+	test('filter to add button as the first toolbar button', () => {})
+	test('filter to add button as the last toolbar button', () => {})
+	test('filter to add button after the prettify button', () => {})
+	test('filter to remove prettify button', () => {})
+	test('filter remove all buttons except a custom button', () => {})
+
+	// this test is not ported over because the history button is no longer
+	// in the toolbar but is over on the left sidebar nav
+	test.skip('it should render with history button', () => {})
+})
