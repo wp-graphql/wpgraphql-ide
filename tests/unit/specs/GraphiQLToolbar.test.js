43583de73762c162643e0dd5183c608191da4557
Jason Bahl <jasonbahl@mac.com>
2024-02-28 10:45:58 -0700

    - scaffold tests for GraphiQLToolbar, marking them as skipped for now

--- tests/unit/specs/GraphiQLToolbar.test.js
+++ tests/unit/specs/GraphiQLToolbar.test.js
@@ -5,16 +5,13 @@
  */
 describe('GraphiQL Toolbar', () => {
 
-	test('it should render', () => {})
+	test.skip('it should render', () => {})
 	test('it should render with prettify button', () => {})
 
-	test('filter to add button as the first toolbar button', () => {})
-	test('filter to add button as the last toolbar button', () => {})
-	test('filter to add button after the prettify button', () => {})
-	test('filter to remove prettify button', () => {})
-	test('filter remove all buttons except a custom button', () => {})
+	test.skip('filter to add button as the first toolbar button', () => {})
+	test.skip('filter to add button as the last toolbar button', () => {})
+	test.skip('filter to add button after the prettify button', () => {})
+	test.skip('filter to remove prettify button', () => {})
+	test.skip('filter remove all buttons except a custom button', () => {})
 
-	// this test is not ported over because the history button is no longer
-	// in the toolbar but is over on the left sidebar nav
-	test.skip('it should render with history button', () => {})
 })
