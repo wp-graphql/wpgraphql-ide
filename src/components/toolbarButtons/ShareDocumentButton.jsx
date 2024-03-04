718f0a2acfb1a927fd5ee26d6978b79d602f424c
Joe Fusco <joe.fusco@wpengine.com>
2024-03-04 14:11:40 -0500

    Fix button styles
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- src/components/toolbarButtons/ShareDocumentButton.jsx
+++ src/components/toolbarButtons/ShareDocumentButton.jsx
@@ -1,77 +1,72 @@
 /* global WPGRAPHQL_IDE_DATA */
 import React from 'react';
-import { useState } from '@wordpress/element';
 import { Icon, external } from '@wordpress/icons';
-import { VisuallyHidden } from '@wordpress/components';
-import { ToolbarButton, useEditorContext } from '@graphiql/react';
+import { VisuallyHidden, ToolbarButton } from '@wordpress/components';
+import { useEditorContext } from '@graphiql/react';
 import LZString from 'lz-string';
+import { useCopyToClipboard } from '../../hooks/useCopyToClipboard';
 
 /**
- * This button would provide a sharable link to users that they can use to share the current
- * query and the user that visits the link would see the same query in the editor.
- *
- * The way I see this working is that the query would be hashed (similar to current WPGraphQL IDE) but instead of persisting the state in the URL as the query changes,
- * it would be hashed when the "share" button is clicked and then appended to the current URL or a generic admin URL that could then be shared.
- *
- * i.e. site.com/wp-admin/admin.php?page=wpgraphql-ide&query=HASHED_QUERY
- *
- * Then GraphiQL would open with the query un-hashed and already set as the current query in the editor.
- *
- * This button, or similar buttons to it could/should be able to be "filtered" into GraphiQL by 3rd parties.
+ * React component for a toolbar button that generates and copies a shareable document link.
+ * 
+ * This component uses the GraphiQL editor context to access the current GraphQL query,
+ * compresses and encodes the query parameters, and generates a shareable URL. It uses the
+ * `useCopyToClipboard` hook to copy this URL to the clipboard and provide user feedback
+ * via WordPress admin notices.
+ * 
+ * @param {Object} props Component properties.
+ * @returns {React.Element} A ToolbarButton element for the share document functionality.
  */
 export const ShareDocumentButton = () => {
-	const { dedicatedIdeBaseUrl } = WPGRAPHQL_IDE_DATA;
-	const { queryEditor } = useEditorContext();
-	const [ shareUrl, setShareUrl ] = useState( '' );
+  const { queryEditor } = useEditorContext();
+  const [copyToClipboard] = useCopyToClipboard();
+  const { dedicatedIdeBaseUrl } = window.WPGRAPHQL_IDE_DATA; 
 
-	const generateShareLink = () => {
-		const query = queryEditor?.getValue() ?? '';
-		const hashedQueryParamObject = getHashedQueryParams({ query });
-		const fullUrl = `${ dedicatedIdeBaseUrl }&wpgraphql_ide=${ hashedQueryParamObject }`;
+  /**
+   * Generates a shareable link for the current document in the editor and copies it to the clipboard.
+   */
+  const generateShareLink = () => {
+    const query = queryEditor?.getValue() ?? '';
+    const hashedQueryParamObject = getHashedQueryParams({ query });
+    const fullUrl = `${dedicatedIdeBaseUrl}&wpgraphql_ide=${hashedQueryParamObject}`;
+    copyToClipboard(fullUrl);
 
-		setShareUrl( fullUrl );
-		copyToClipboard( fullUrl );
-	};
+    // TODO: notify user that a shareable link is copied to clipboard
+  };
 
-	return (
-		<ToolbarButton
-			onClick={ generateShareLink }
-			label="Share current document"
-		>
-			<Icon
-				icon={ external }
-				style={ {
-					fill: 'hsla(var(--color-neutral), var(--alpha-tertiary))',
-				} }
-			/>
-			<VisuallyHidden>Share</VisuallyHidden>
-		</ToolbarButton>
-	);
+  return (
+    <ToolbarButton
+      className="graphiql-un-styled graphiql-toolbar-button"
+      onClick={generateShareLink}
+      label="Share current document"
+    >
+		<Icon
+			icon={ external }
+			style={ {
+				fill: 'hsla(var(--color-neutral), var(--alpha-tertiary))',
+			} }
+		/>
+		<VisuallyHidden>Share</VisuallyHidden>
+    </ToolbarButton>
+  );
 };
 
-async function copyToClipboard( text ) {
-	console.log( 'Copied: ', text );
-}
-
 /**
- * Converts a given object to a compressed, encoded URI component string using LZString compression.
- * This is useful for generating shorter query parameters from complex objects.
+ * Compresses and encodes a query parameter object for use in a shareable URL.
  * 
- * @param {Object} obj - The object to be converted into a compressed query parameter.
- * @returns {string} The compressed and encoded URI component string.
- * @throws {TypeError} If the input is not an object or cannot be serialized.
+ * @param {Object} obj The object containing query parameters to be compressed and encoded.
+ * @returns {string} A compressed and encoded string representing the query parameters.
  */
-export function getHashedQueryParams(obj) {
-    if (typeof obj !== 'object' || obj === null) {
-        console.error('Input must be a non-null object');
-		return null;
-    }
-
-    try {
-        const queryParamString = JSON.stringify(obj);
-        const compressedQueryParamShareObject = LZString.compressToEncodedURIComponent(queryParamString);
-        return compressedQueryParamShareObject;
-    } catch (error) {
-        console.error('Failed to compress query parameter object:', error);
-    }
+function getHashedQueryParams(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    console.error('Input must be a non-null object');
+    return '';
+  }
+  try {
+    const queryParamString = JSON.stringify(obj);
+    return LZString.compressToEncodedURIComponent(queryParamString);
+  } catch (error) {
+    console.error('Failed to compress query parameter object:', error);
+    return '';
+  }
 }
