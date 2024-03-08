83e51623f9144054ebe957ae778bcb5dc957a889
Jason Bahl <jasonbahl@mac.com>
2024-03-08 12:09:39 -0700

    - showing the explorerPlugin() not working as a 3rd party extension

--- plugins/query-composer/index.js
+++ plugins/query-composer/index.js
@@ -1,19 +1,29 @@
 import { registerPlugin } from 'wpgraphql-ide';
+import { addAction } from '@wordpress/hooks';
 import { explorerPlugin } from "@graphiql/plugin-explorer";
+
 import { Icon, pencil } from '@wordpress/icons';
 import { QueryComposer } from './components/QueryComposer'
 
 
-registerPlugin( 'queryComposer', {
-	title: 'Query Composer',
-	icon: () => (
-		<Icon
-			icon={ pencil }
-			style={ {
-				fill: 'hsla(var(--color-neutral), var(--alpha-tertiary))',
-			} }
-		/>
-	),
-	content: () => <QueryComposer />
-} );
+// registerPlugin( 'queryComposer', {
+// 	title: 'Query Composer',
+// 	icon: () => (
+// 		<Icon
+// 			icon={ pencil }
+// 			style={ {
+// 				fill: 'hsla(var(--color-neutral), var(--alpha-tertiary))',
+// 			} }
+// 		/>
+// 	),
+// 	content: () => <QueryComposer />
+// } );
+
+addAction( 'wpgraphqlide_rendered', 'wpgraphql-ide-query-composer',() => {
+	console.log({
+		wpgraphqlide_rendered: true
+	})
+	registerPlugin( 'queryComposer', explorerPlugin() );
+});
+
 
