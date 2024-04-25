8e0ff36e88d240eb84929a4bc2c9c4ee749d4d80
Joe Fusco <joe.fusco@wpengine.com>
2024-04-25 15:01:47 -0400

    Defer script to assist with lowering the drawer delay on some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* global WPGRAPHQL_IDE_DATA */
-import { createRoot, useEffect } from '@wordpress/element';
+import { useEffect } from '@wordpress/element';
 import { doAction } from '@wordpress/hooks';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { parse, print } from 'graphql';
@@ -8,7 +8,7 @@ import LZString from 'lz-string';
 import { EditorDrawer } from './components/EditorDrawer';
 import { Editor } from './components/Editor';
 
-const { isDedicatedIdePage } = window.WPGRAPHQL_IDE_DATA;
+const { isDedicatedIdePage, context: { drawerButtonLabel } } = window.WPGRAPHQL_IDE_DATA;
 
 const url = new URL( window.location.href );
 const params = url.searchParams;
@@ -129,7 +129,7 @@ export function RenderApp() {
 
 	return (
 		<div className="AppRoot">
-			<EditorDrawer>
+			<EditorDrawer buttonLabel={drawerButtonLabel}>
 				<Editor />
 			</EditorDrawer>
 		</div>
--- src/index.js
+++ src/index.js
@@ -1,37 +1,57 @@
-// WordPress dependencies for hooks, data handling, and component rendering.
+// Import WordPress dependencies for hooks, data handling, and component rendering.
 import { createHooks } from '@wordpress/hooks';
 import { register } from '@wordpress/data';
-import { createRoot } from '@wordpress/element';
+import { render, createRoot } from '@wordpress/element';
 import * as GraphQL from "graphql/index.js";
 
-// Local imports including the store configuration and the main App component.
+// Import local store configuration and the main App component.
 import { store } from './store';
 import { App } from './App';
 
-// Register the store with wp.data to make it available throughout the plugin.
-register( store );
+/**
+ * Registers the store with WordPress data module to make it globally accessible.
+ */
+register(store);
 
-// Create a central event hook system for the WPGraphQL IDE.
+/**
+ * Creates a central event hook system for the WPGraphQL IDE, facilitating the extension
+ * of functionality through external scripts and internal hooks.
+ */
 export const hooks = createHooks();
 
-// Expose a global variable for the IDE, facilitating extension through external scripts.
+/**
+ * Exposes a global IDE variable, making it accessible for extensions and external scripts.
+ */
 window.WPGraphQLIDE = {
-	hooks,
-	store,
-	GraphQL
+    hooks,
+    store,
+    GraphQL
 };
 
 /**
- * Initialize and render the application once the DOM is fully loaded.
- * This ensures that the application mounts when all page elements are available.
+ * Function to abstract the differences in rendering methods between different versions of React.
+ * Uses `createRoot` if available, falling back to `render` if not, to accommodate different React versions.
+ *
+ * @param {HTMLElement} root - The DOM element where the React component will be mounted.
+ * @param {ReactElement} component - The React component to render.
  */
-document.addEventListener( 'DOMContentLoaded', () => {
-	const appMountPoint = document.getElementById( 'wpgraphql-ide-root' );
-	if ( appMountPoint ) {
-		createRoot( appMountPoint ).render( <App /> );
-	} else {
-		console.error(
-			'WPGraphQL IDE mount point not found. Please ensure an element with ID "wpgraphql-ide-root" exists.'
-		);
-	}
-} );
+function compatibleRender(root, component) {
+    if (typeof createRoot !== 'undefined') {
+        createRoot(root).render(component);
+    } else {
+        render(component, root);
+    }
+}
+
+/**
+ * Immediately attempt to render the React application to the designated mount point.
+ * This assumes that the script is loaded with 'defer', ensuring the DOM is parsed beforehand.
+ */
+const appMountPoint = document.getElementById('wpgraphql-ide-root');
+if (appMountPoint) {
+    compatibleRender(appMountPoint, <App />);
+} else {
+    console.error(
+        'WPGraphQL IDE mount point not found. Please ensure an element with ID "wpgraphql-ide-root" exists.'
+    );
+}
