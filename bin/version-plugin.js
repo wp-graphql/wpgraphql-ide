9a80a042035280231c34a61668f562c182ead603
Joe Fusco <joe.fusco@wpengine.com>
2024-06-13 10:33:10 -0400

    Fix versioning script to include version constant

--- bin/version-plugin.js
+++ bin/version-plugin.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * Versions a WordPress plugin.
- *
- * Ported over from FaustJS
- * @link https://github.com/wpengine/faustjs/blob/canary/scripts/versionPlugin.js
- */
 const fs = require( 'fs/promises' );
 const path = require( 'node:path' );
 
@@ -43,7 +37,7 @@ async function versionPlugin() {
 async function bumpPluginHeader( pluginFile, version ) {
 	return bumpVersion(
 		pluginFile,
-		/^\s*\*\s*Version:\s*([0-9.]+)$/gm,
+		/^\s*\*\s*Version:\s*([0-9.]+)\s*$/gm,
 		version
 	);
 }
@@ -60,7 +54,7 @@ async function bumpStableTag( readmeTxt, version ) {
 }
 
 /**
- * Updates the version constant found in the WPGraphQLContentBlocks.php file.
+ * Updates the version constant found in the wpgraphql-ide.php file.
  *
  * @param {string} pluginFile Full path to a file containing PHP constants.
  * @param {string} version    The new version number.
@@ -68,7 +62,7 @@ async function bumpStableTag( readmeTxt, version ) {
 async function bumpVersionConstant( pluginFile, version ) {
 	return bumpVersion(
 		pluginFile,
-		/^\s*define\(\s*'WPGRAPHQL_IDE_VERSION',\s*'([0-9.]+)'\s*\);/,
+		/^\s*define\(\s*'WPGRAPHQL_IDE_VERSION',\s*'([0-9.]+)'\s*\);/gm,
 		version
 	);
 }
@@ -115,7 +109,7 @@ async function bumpVersion( file, regex, version ) {
  *
  * @param {string} pluginPath Full path to the directory containing the plugin's
  *                            package.json file.
- * @return The version number string found in the plugin's package.json.
+ * @return {string|undefined} The version number string found in the plugin's package.json.
  */
 async function getNewVersion( pluginPath ) {
 	const packageJsonFile = path.join( pluginPath, 'package.json' );
